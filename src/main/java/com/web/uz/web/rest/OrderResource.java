a5fcbe200bfa18da595cd2bd462aabb39e846e8c
a.yallaev <a.yallayev@hlsys.uz>
2021-09-15 09:27:45 +0500

    full reactor save db two table

--- src/main/java/com/web/uz/web/rest/OrderResource.java
+++ src/main/java/com/web/uz/web/rest/OrderResource.java
@@ -1,23 +1,25 @@
 package com.web.uz.web.rest;
 
-import com.web.uz.domain.DetailForOrder;
 import com.web.uz.domain.Order;
 import com.web.uz.repository.OrderRepository;
-import com.web.uz.service.OrderV2Service;
-import com.web.uz.service.dto.OrderDto;
-
-import java.math.BigDecimal;
+import com.web.uz.web.rest.errors.BadRequestAlertException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-
-import liquibase.pro.packaged.E;
+import java.util.Objects;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.reactive.ResponseUtil;
 
 /**
@@ -31,11 +33,9 @@ public class OrderResource {
     private final Logger log = LoggerFactory.getLogger(OrderResource.class);
 
     private final OrderRepository orderRepository;
-    private final OrderV2Service orderV2Service;
 
-    public OrderResource(OrderRepository orderRepository, OrderV2Service orderV2Service) {
+    public OrderResource(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
-        this.orderV2Service = orderV2Service;
     }
 
     /**
@@ -70,16 +70,5 @@ public class OrderResource {
         log.debug("REST request to get Order : {}", id);
         Mono<Order> order = orderRepository.findById(id);
         return ResponseUtil.wrapOrNotFound(order);
-    }
-
-    @PostMapping("/order/create")
-    public Mono<?> createOrder(@RequestBody OrderDto orderDto){
-
-        return orderV2Service.createOrder(orderDto);
-//        Mono<DetailForOrder> order = orderV2Service.createOrder(orderDto);
-//        return ResponseEntity.ok(order.switchIfEmpty(Mono.error(new Exception("error by Aslom")))
-//            .flatMap(Mono::just));
-//        return order.flatMap(Mono::just)
-//            .onErrorResume(ex -> Mono.error(new Exception(ex)));
     }
 }
